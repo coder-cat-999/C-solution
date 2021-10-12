64965dc4fec7e2e182543eb4c24186b30a798ec8
coder-cat-999 <92074116+coder-cat-999@users.noreply.github.com>
2021-10-12 11:20:30 +0530

    Solution matrix Addition

--- Solutions/MatrixAddition.c
+++ Solutions/MatrixAddition.c
@@ -2,31 +2,36 @@
 
 int main()
 {
-    int mat1[4][4] , mat2[4][4];
+    int row, col;
+	printf("enter row size of both matrices to be added :\n");
+	scanf("%d",&row);
+	printf("enter column size of both matrices to be added :\n");
+	scanf("%d",&col);
     
+	int mat1[row][col], mat2[row][col];
     printf("Enter elemtents in Matrix 1\n");
-    for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
             scanf("%d",&mat1[i][j]);
         }
     }
     
     printf("Enter elemtents in Matrix 2\n");
-    for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
             scanf("%d",&mat2[i][j]);
         }
     }
     
-    int sum_Matrix[4][4];
-    for( int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
+    int sum_Matrix[row][col];
+    for( int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
             sum_Matrix[i][j] = mat1[i][j] + mat2[i][j];
         }
     }
     printf("Output: \n");
-    for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
             printf("%d ", sum_Matrix[i][j]);
         }printf("\n");
     }
